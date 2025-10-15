fbcbf1603a0b76780d08476a987fb17aaef95dcb
Lucas Gaitón <gaitonlucas7@gmail.com>
2025-10-15 19:44:50 -0300

    commit diario

--- MambaPDF/App.js
+++ MambaPDF/App.js
@@ -6,6 +6,7 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.js to start working on your vdfapp!j877ojo xD</Text>
       <StatusBar style="auto" />
+      dawdwadwadawd
     </View>
   );
 }

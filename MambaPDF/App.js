4f2958e45f890f0a699c46ea50646622b3197be4
Lucas Gaitón <gaitonlucas7@gmail.com>
2025-10-21 19:02:10 -0300

    commit diario

--- MambaPDF/App.js
+++ MambaPDF/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your vdfapp!j877ojo xD</Text>
+      <Text>Open up App.js to start working on your vdfapp!j877ojo xDD</Text>
       <StatusBar style="auto" />
       dawdwadwadawd
     </View>

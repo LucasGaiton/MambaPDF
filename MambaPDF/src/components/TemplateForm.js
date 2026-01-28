9687cff879a19262823f27ffea47c01a01d5f361
Lucas Gaitón <gaitonlucas7@gmail.com>
2026-01-28 00:03:21 -0300

    Inicialización de las pruebas de el localstorage

--- MambaPDF/src/components/TemplateForm.js
+++ MambaPDF/src/components/TemplateForm.js
@@ -1,3 +1,12 @@
-export default templateForm = ()=>{
+import { View, Text, TextInput, Button, FlatList, StyleSheet } from "react-native"
+import { useState, useEffect } from "react"
+import { guardarPlantilla } from "../storage/storage"
+export default function templateForm () {
     
+
+
+    return(
+        <></>
+    )
+
 }
